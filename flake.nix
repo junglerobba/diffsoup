2a9e62ea26fa13156a939c945cd38d1b75d6fb5c
Tobias Langendorf <junglerobba@jngl.one>
2026-08-07 23:33:53 +0200

    actions: run via nix flake checks
    A standard nix package via rustPlatform.buildRustPackage already
    compiles and runs tests, and this way it will also make sure that any
    required dependencies are also present for the nix package, not just in
    the devshell.
    The other two (formatting and clippy) are done via flake checks, which
    both override just the relevant attrs on the actual package, so crates
    will already be present.
    
    This doesn't run as separate steps or anything, just as a simple quality
    gate, where log content will show what failed.

--- flake.nix
+++ flake.nix
@@ -12,8 +12,9 @@
       ...
     }:
     flakelight ./. (
-      { lib, ... }:
+      { lib, outputs, ... }:
       {
+        flakelight.builtinFormatters = false;
         systems = lib.systems.flakeExposed;
         package =
           pkgs:
@@ -21,6 +22,28 @@
             shortRev = self.shortRev or "dirty";
           };
         devShell = import ./shell.nix;
+        checks = {
+          format =
+            pkgs:
+            outputs.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (prev: {
+              pname = "${prev.pname}-format";
+              dontBuild = true;
+              doCheck = true;
+              nativeCheckInputs = [ pkgs.rustfmt ];
+              checkPhase = "cargo fmt --check";
+              installPhase = "touch $out";
+            });
+          clippy =
+            pkgs:
+            outputs.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (prev: {
+              pname = "${prev.pname}-clippy";
+              dontBuild = true;
+              doCheck = true;
+              nativeCheckInputs = [ pkgs.clippy ];
+              checkPhase = "cargo clippy --all-targets -- -Dwarnings";
+              installPhase = "touch $out";
+            });
+        };
       }
     );
 }
